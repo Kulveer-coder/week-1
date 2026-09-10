4bf1044f11bb8098a0dcfef0c638466c0a8ebd41
Kulveer Kaur <kk76@iu.edu>
2026-09-09 22:30:52 -0400

    Add palindrome and parentheses validation functions

--- apputil.py
+++ apputil.py
@@ -1,3 +1,22 @@
 
 
 # add code below ...
+def palindrome (word):
+    """check whether a word or phrase is a palindrome."""
+    clean_word = ""
+    for character in word.lower():
+        if character.isalnum():
+            clean_word += character
+    return clean_word == clean_word[::-1]
+
+
+def parentheses(sequence):
+    stack = []
+    for char in sequence:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
